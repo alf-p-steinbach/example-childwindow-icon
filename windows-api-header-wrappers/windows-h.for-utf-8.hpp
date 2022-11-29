37f00c6be0e0f94ca43703de9de7087232754a32
alf-p-steinbach <alf.p.steinbach@gmail.com>
2022-11-29 23:57:34 +0100

    oh

---
+++ windows-api-header-wrappers/windows-h.for-utf-8.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#undef UTF8_WINAPI
+#define UTF8_WINAPI
+#include <windows-api-header-wrappers/windows-h.hpp>
+
+namespace uuid_0985060C_1AAD_453C_B3F9_A2E543F4CF1E {
+    struct Winapi_envelope
+    {
+        Winapi_envelope()
+        {
+            static const bool dummy = winapi_h_assert_utf8_codepage();
+        }
+    };
+    
+    static const Winapi_envelope ensured_globally_single_utf8_assertion{};
+}  // namespace uuid_0985060C_1AAD_453C_B3F9_A2E543F4CF1E
